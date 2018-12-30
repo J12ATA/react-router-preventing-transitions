7734a8179c173599714eb2b722dd9ae360c67c69
Jose Figueroa <Josefigueroalv@protonmail.com>
2018-12-30 12:44:11 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Prompt
+} from 'react-router-dom'
+
+class Form extends React.Component {
+  state = {
+    isBlocking: false,
+  }
+  render() {
+    const {isBlocking} = this.state
+    
+    return (
+      <form onSubmit={(event) => {
+        event.preventDefault()
+        event.target.reset()
+        this.setState(() => ({
+          isBlocking: false
+        }))
+      }}>
+        <Prompt 
+          when={isBlocking}
+          message={(location) => `Are you sure you want to go to ${location.pathname}`}
+        />
+
+        <p>
+          Blocking? {isBlocking === true
+            ? 'Yes, click a link or the back button.'
+            : 'Nope'}
+        </p>
+
+        <p>
+          <input
+            size="50"
+            placeholder="type something to block transition"
+            onChange={(event) => {
+              const isBlocking = event.target.value.length > 0
+              this.setState(() => ({
+                isBlocking
+              }))
+            }}
+          />
+        </p>
+
+        <p>
+          <button>Submit to stop blocking</button>
+        </p>
+      </form>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <li><Link to='/'>Form</Link></li>
+              <li><Link to='/one'>One</Link></li>
+              <li><Link to='/two'>Two</Link></li>
+            </li>
+          </ul>
+
+          <Route path='/' exact component={Form} />
+          <Route path='/one' render={() => <h3>One</h3>} />
+          <Route path='/two' render={() => <h3>Two</h3>} />
+        </div>
+      </Router>
+    )
   }
 }
 
